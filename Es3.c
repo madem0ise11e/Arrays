f1f4aaee923f15fc46857f6e8889a9a02f7d55f9
Tetiana Kizan <tetiana.kizan@gmail.com>
2020-02-05 20:59:02 +0100

    Update es3.c

--- Es3.c
+++ Es3.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#define MAXIMUM 100
 
+int main() {
+  int n, a[MAXIMUM], b[MAXIMUM], c[MAXIMUM];
+  
+  printf("Quanti numeri vuoi inserire? (max %d)", MAXIMUM);
+  scanf("%d", &n);
+  
+  
